3b84bf6450f65906345b62716f6a9b30ce35e315
goralskd <goralskd@email.sc.edu>
2024-12-06 23:29:34 -0500

    Update ChocolateBarCounter.java

--- ChocolateBarCounter.java
+++ ChocolateBarCounter.java
@@ -2,7 +2,7 @@ package lab5;
 
 import java.util.Scanner;
 
-public class Lab05 {
+public class ChocolateBarCounter {
 	    public static void main(String[] args) {
 	        Scanner scanner = new Scanner(System.in);
 	        
